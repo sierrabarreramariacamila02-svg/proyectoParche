c9e6d61fbdebcd18b4fb488c975305368044a680
ericbertodiaz10-ux <ericbertodiaz10@gmail.com>
2026-08-26 15:25:25 -0500

    agregar variaciones del rol admin, administracion y admini,para evitar bloqueos, en middleware de productos

--- routes/producto.js
+++ routes/producto.js
@@ -7,8 +7,10 @@ const router = express.Router();
 router.get('/', productoController.listar);
 router.get('/disponibles', productoController.listarDisponibles);
 router.get('/:id', productoController.obtenerPorId);
-router.post('/', verificarToken, verificarRol('admin'), upload.single('imagen'), productoController.crear);
-router.put('/:id', verificarToken, verificarRol('admin'), upload.single('imagen'), productoController.actualizar);
-router.delete('/:id', verificarToken, verificarRol('admin'), productoController.eliminar);
+
+// Permitimos 'admin', 'administración' y 'admini' para evitar bloqueos por tipado en BD
+router.post('/', verificarToken, verificarRol('admin', 'administración', 'admini'), upload.single('imagen'), productoController.crear);
+router.put('/:id', verificarToken, verificarRol('admin', 'administración', 'admini'), upload.single('imagen'), productoController.actualizar);
+router.delete('/:id', verificarToken, verificarRol('admin', 'administración', 'admini'), productoController.eliminar);
 
 export default router;
