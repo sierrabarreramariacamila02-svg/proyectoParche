e6fcf6d2a03854b89a2974128afd367fc62b0a65
carloschavarroome-art <carloschavarroome@gmail.com>
2026-09-15 06:36:57 -0500

    subir googleauth

--- backend/controllers/googleAuth.js
+++ backend/controllers/googleAuth.js
@@ -77,6 +77,7 @@ export const autenticarConGoogle = async (req, res) => {
                 avatar: usuarioFinal.avatar || avatar
             }
         });
+        
 
     } catch (error) {
         console.error("Error en autenticarConGoogle:", error);
